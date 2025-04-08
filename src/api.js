b22491f73358f176901e3d6d81b6e71abbb5656e
Thenvanathi <thenvanathi2003@gmail.com>
2025-04-08 18:39:56 +0530

    🔗 Connected frontend with backend

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
-const API_BASE_URL = 'https://recipeapp-backend-9kd6.onrender.com';
+const API_BASE_URL = 'https://recipeapp-backend-9kd6.onrender.com/';
 
 export async function searchRecipes(query) {
-    try{
-        const response = await fetch(`${API_BASE_URL}/search.php?s=${query}`);
-        const data = await response.json();
-        return data.meals; 
-    } catch (error) {
-        console.error('Error fetching recipes:', error);
-        return null; 
-    }
+  try {
+    //const response = await fetch(`${API_BASE_URL}/search?q=${query}`);
+    const response = await fetch(`${API_BASE_URL}/search?s=${query}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching recipes:', error);
+    return null;
+  }
 }
