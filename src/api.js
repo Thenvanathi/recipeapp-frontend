e0ae2c84814aa2baf2adb5e03751b3221eb640ba
Thenvanathi <thenvanathi2003@gmail.com>
2025-04-08 19:02:21 +0530

    Initial commit for recipe app frontend

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-const API_BASE_URL = 'https://recipeapp-backend-9kd6.onrender.com/';
+// Use your own backend as a proxy
+const API_BASE_URL = 'https://recipeapp-backend-9kd6.onrender.com';
 
 export async function searchRecipes(query) {
   try {
-    //const response = await fetch(`${API_BASE_URL}/search?q=${query}`);
-    const response = await fetch(`${API_BASE_URL}/search?s=${query}`);
+    const response = await fetch(`${API_BASE_URL}/search?query=${query}`);
     const data = await response.json();
-    return data;
+    return data.meals;
   } catch (error) {
     console.error('Error fetching recipes:', error);
     return null;
